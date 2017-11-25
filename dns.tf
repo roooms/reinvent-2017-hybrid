ab385c70f190aae7f4512b1502f970b053cb904f
Dan Brown <roooms@gmail.com>
2017-11-25 15:09:39 +0000

    Add weighted DNS via Route53

---
+++ dns.tf
@@ -0,0 +1,59 @@
+data "aws_route53_zone" "hashidemos" {
+  name = "hashidemos.io."
+}
+
+resource "aws_route53_record" "hashidemos_gcp" {
+  zone_id = "${data.aws_route53_zone.hashidemos.zone_id}"
+  name    = "hybrid-cloud-gcp.hashidemos.io."
+  type    = "A"
+  ttl     = "300"
+  records = ["${module.gcp_lb_http.external_ip}"]
+}
+
+resource "aws_route53_record" "hashidemos_aws" {
+  zone_id = "${data.aws_route53_zone.hashidemos.zone_id}"
+  name    = "hybrid-cloud-aws.hashidemos.io."
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${module.aws_elb.this_elb_dns_name}"]
+}
+
+resource "aws_route53_record" "hybrid-cloud-aws" {
+  zone_id = "${data.aws_route53_zone.hashidemos.zone_id}"
+  name    = "hybrid-cloud.hashidemos.io."
+  type    = "CNAME"
+  ttl     = "5"
+
+  weighted_routing_policy {
+    weight = 50
+  }
+
+  set_identifier = "aws"
+  records        = ["hybrid-cloud-aws.hashidemos.io"]
+}
+
+resource "aws_route53_record" "hybrid-cloud-gcp" {
+  zone_id = "${data.aws_route53_zone.hashidemos.zone_id}"
+  name    = "hybrid-cloud.hashidemos.io."
+  type    = "CNAME"
+  ttl     = "5"
+
+  weighted_routing_policy {
+    weight = 50
+  }
+
+  set_identifier = "gcp"
+  records        = ["hybrid-cloud-gcp.hashidemos.io"]
+}
+
+output "aws-dns" {
+  value = "${aws_route53_record.hashidemos_aws.fqdn}"
+}
+
+output "gcp-dns" {
+  value = "${aws_route53_record.hashidemos_gcp.fqdn}"
+}
+
+output "0-dns" {
+  value = "hybrid-cloud.hashidemos.io"
+}
